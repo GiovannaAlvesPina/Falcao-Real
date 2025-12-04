9e376944d657977097317c1af704fd53cddfe645
Giovanna Alves Pina <giovanna.pina@sptech.school>
2025-12-03 23:03:21 -0300

    Trocando o type da input de dtNascimento para date

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -20,20 +20,6 @@ function autenticar(req, res) {
                         console.log(resultadoAutenticar);
                         res.json(resultadoAutenticar[0]);
 
-               /*         aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].idUsuario,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                       // senha: resultadoAutenticar[0].senha,
-                                     //   aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })*/
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
